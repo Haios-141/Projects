925b7381ba40e9cd7cfcb51a90d9e977e04a9845
Haios-141 <34633341+Haios-141@users.noreply.github.com>
2023-04-11 23:26:27 +0200

    Add files via upload

---
+++ Python/Basic/1 - Treasure Island/main.py
@@ -0,0 +1,125 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+action = input("After walking for a long time, you reach a crossroad. Where do you want to go; left or right?\n").lower()
+
+if action == "left":
+    print("\n")
+
+    action = input("As you continue walking, you come across a lake with an island in the middle. You notice a man on a small boat paddling towards the island. It's going to be a while before he comes back. Are you going to swim across or wait for the boat? Type \"Swim\" or \"Wait\"\n").lower()
+
+    if action == "wait":
+        print("\n")
+
+        colour = input("You arrive on the island, and find three differently coloured doors, not attached to anything. The doors are coloured; Red, Yellow and Blue. Which do you choose?\n").lower()
+
+        print("\n")
+
+        if colour == "yellow":
+            print("You open the door and are magically transported to a room with treasure. Congratulations!!! YOU WIN!!!!")
+        elif colour == "red":
+            print("You are transported to a room with fire running towards you. Game over.")
+        elif colour == "blue":
+            print("You are transported to a room with beasts. They attack and eat you. Game over.")
+
+            print('''
+                                                          ,--,  ,.-.
+                ,                   \,       '-,-`,'-.' | ._
+               /|           \    ,   |\         }  )/  / `-,',
+               [ '          |\  /|   | |        /  \|  |/`  ,`
+               | |       ,.`  `,` `, | |  _,...(   (      _',
+               \  \  __ ,-` `  ,  , `/ |,'      Y     (   \_L\
+                \  \_\,``,   ` , ,  /  |         )         _,/
+                 \  '  `  ,_ _`_,-,<._.<        /         /
+                  ', `>.,`  `  `   ,., |_      |         /
+                    \/`  `,   `   ,`  | /__,.-`    _,   `\
+                -,-..\  _  \  `  /  ,  / `._) _,-\`       \
+                 \_,,.) /\    ` /  / ) (-,, ``    ,        |
+                ,` )  | \_\       '-`  |  `(               \
+               /  /```(   , --, ,' \   |`<`    ,            |
+              /  /_,--`\   <\  V /> ,` )<_/)  | \      _____)
+        ,-, ,`   `   (_,\ \    |   /) / __/  /   `----`
+       (-, \           ) \ ('_.-._)/ /,`    /
+       | /  `          `/ \\ V   V, /`     /
+    ,--\(        ,     <_/`\\     ||      /
+   (   ,``-     \/|         \-A.A-`|     /
+  ,>,_ )_,..(    )\          -,,_-`  _--`
+ (_ \|`   _,/_  /  \_            ,--`
+  \( `   <.,../`     `-.._   _,-`
+   `                      ```
+            ''')
+        else:
+            print("Game over.")
+    else:
+        print("\n")
+
+        print("You swim across the lake and see what has to be an impossible sight; a giant shark - a shark in a lake. You turn around and try to swim back to land, but the shark catches up to you, and devours you. Game over.")
+        
+        print('''
+                                         ,-
+                               ,'::|
+                              /::::|
+                            ,'::::o\                                      _..
+         ____........-------,..::?88b                                  ,-' /
+ _.--"""". . . .      .   .  .  .  ""`-._                           ,-' .;'
+<. - :::::o......  ...   . . .. . .  .  .""--._                  ,-'. .;'
+ `-._  ` `":`:`:`::||||:::::::::::::::::.:. .  ""--._ ,'|     ,-'.  .;'
+     """_=--       //'doo.. ````:`:`::::::::::.:.:.:. .`-`._-'.   .;'
+         ""--.__     P(       \               ` ``:`:``:::: .   .;'
+                "\""--.:-.     `.                             .:/
+                  \. /    `-._   `.""-----.,-..::(--"".\""`.  `:\
+                   `P         `-._ \          `-:\          `. `:\
+                                   ""            "            `-._)
+        ''')
+
+else:
+    print("\n")
+
+    print("After going through a bunch of thick trees and greenery, you fall into a wormhole and disappear. Game over.")
+
+    print('''
+           ________________________________         
+      /                                "-_          
+     /      .  |  .                       \          
+    /      : \ | / :                       \         
+   /        '-___-'                         \      
+  /_________________________________________ \      
+       _______| |________________________--""-L 
+      /       F J                              \ 
+     /       F   J                              L
+    /      :'     ':                            F
+   /        '-___-'                            / 
+  /_________________________________________--"  
+    ''')
+    
+
+
+
+
+
